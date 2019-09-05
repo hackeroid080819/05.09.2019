c4a94976a3b310a67433b3c8908965e9772744ba
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-05 11:06:24 +0300

    Update MainActivity.java

--- newclick/MainActivity.java
+++ newclick/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+// now the entire activity implements the View.OnCLickListener
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
